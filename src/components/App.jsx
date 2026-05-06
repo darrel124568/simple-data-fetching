7958b0bf398102aaf24aab067cc18cb4bd98566d
darrel124568 <darrel124568@gmail.com>
2026-05-06 08:38:13 +0300

    simple-fetch-lab

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,1 +1,26 @@
-// create your App component here
+import React,{ useState, useEffect } from "react";
+
+export default function App() {
+const [image, setimage] = useState('')
+const [loading, setloading] = useState(true)
+const [clicked, setclicked] = useState(0)
+useEffect(()=> {
+setloading(true)
+fetch('https://dog.ceo/api/breeds/image/random')
+.then(response => response.json())
+.then(data => {
+    setimage(data.message)
+    setloading(false)
+})
+},[clicked])
+function handleClick() {
+ setclicked(Math.random())
+}
+return (
+    <div>
+    {loading && <p>Loading...</p>}
+    {!loading && <img src={image} alt="A Random Dog"/>}
+    <button onClick={() => handleClick()}>Fetch new image</button>
+    </div>
+)
+}
